33fe80e9ecac67ec7a3fc33274f972658bd629f1
Qleoz12 <qleoz12.leonardo@gmail.com>
2020-02-02 23:38:49 -0500

    changes of menu and the pages will no longer used

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,7 +9,7 @@ export class AppConstants {
 
   public static DATE = '2019-07-01';
   public static APP_TITLE = 'DANN';
-  public static APP_TITLE_VERSION: string = AppConstants.APP_TITLE + ' - ' + AppConstants.DATE;
+  public static APP_TITLE_VERSION: string = AppConstants.APP_TITLE;
 
   public static ANGULAR_VERSION = 'Ng ' + VERSION.full;
   public static BOOTSTRAP_VERSION = $.fn.tooltip.Constructor.VERSION;
